4afb51a32a6c49f61ef7621545af6eb712db01bc
Sahitya Remella <sahityaremella@gmail.com>
2024-01-01 14:35:48 +0530

    initial commit

---
+++ ex1.js
@@ -0,0 +1,8 @@
+// function hello(){
+//     console.log("hello, my first nodejs script");
+// }
+// hello();
+const hello = () =>{
+    console.log("hello, trying for the first time");
+}
+hello();
